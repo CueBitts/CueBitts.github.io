7a647ee41fcf5c7c3db54417c2a38910b210a646
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-03-08 08:25:16 -0800

    dot grid exists

---
+++ script.js
@@ -0,0 +1,6 @@
+for(i=0; i<25; i++) {
+    dot = document.createElement('div');
+    dot.setAttribute('id', 'dot');
+    dotGrid = document.getElementById('dotGrid')
+    dotGrid.appendChild(dot);
+}
