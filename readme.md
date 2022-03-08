9c8841e0a2df2e74d3555ac0a4401f540b325491
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-03-08 09:31:56 -0800

    can click to add line

--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ To start I used JS to create 25 dots. I then styled them with CSS Grid to organi
 
 vertical grid lines exist
 I used a similar process to make the vertical lines of the grid. They will have to be seperate elements to check if they have been clicked.
+
+horizontal grid lines exist
+Same process as that for adding the vertical grid lines. 
--- script.js
+++ script.js
@@ -9,10 +9,18 @@ for(let i=0; i<20; i++) {
     vrt.setAttribute('id', 'vrt');
     vrtGrid = document.getElementById('vrtGrid');
     vrtGrid.appendChild(vrt);
+
+    vrt.addEventListener('click', clk);
 }
 for(let i=0; i<20; i++) {
     hrz = document.createElement('div');
     hrz.setAttribute('id', 'hrz');
     hrzGrid = document.getElementById('hrzGrid');
     hrzGrid.appendChild(hrz);
+
+    hrz.addEventListener('click', clk);
+}
+
+function clk() {
+    this.style.background = 'red';
 }
