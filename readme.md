c29daab863e1df99421a32e711c3acfce36c1029
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-03-08 08:42:58 -0800

    vertical grid lines exist

---
+++ readme.md
@@ -0,0 +1,2 @@
+dot grid exists
+To start I used JS to create 25 dots. I then styled them with CSS Grid to organize them into a grid.
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 for(i=0; i<25; i++) {
     dot = document.createElement('div');
     dot.setAttribute('id', 'dot');
-    dotGrid = document.getElementById('dotGrid')
+    dotGrid = document.getElementById('dotGrid');
     dotGrid.appendChild(dot);
+}
+for(i=0; i<20; i++) {
+    vrt = document.createElement('div');
+    vrt.setAttribute('id', 'vrt');
+    vrtGrid = document.getElementById('vrtGrid');
+    vrtGrid.appendChild(vrt);
 }
