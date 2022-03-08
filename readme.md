52da14f83dfec58dfee4ff4010c89ceaf8a0139b
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-03-08 09:25:08 -0800

    horizontal grid lines exist

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 dot grid exists
 To start I used JS to create 25 dots. I then styled them with CSS Grid to organize them into a grid.
+
+vertical grid lines exist
+I used a similar process to make the vertical lines of the grid. They will have to be seperate elements to check if they have been clicked.
--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-for(i=0; i<25; i++) {
+for(let i=0; i<25; i++) {
     dot = document.createElement('div');
     dot.setAttribute('id', 'dot');
     dotGrid = document.getElementById('dotGrid');
     dotGrid.appendChild(dot);
 }
-for(i=0; i<20; i++) {
+for(let i=0; i<20; i++) {
     vrt = document.createElement('div');
     vrt.setAttribute('id', 'vrt');
     vrtGrid = document.getElementById('vrtGrid');
     vrtGrid.appendChild(vrt);
+}
+for(let i=0; i<20; i++) {
+    hrz = document.createElement('div');
+    hrz.setAttribute('id', 'hrz');
+    hrzGrid = document.getElementById('hrzGrid');
+    hrzGrid.appendChild(hrz);
 }
